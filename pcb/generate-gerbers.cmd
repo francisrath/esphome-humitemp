30ef23d58e70628d3595c024e883647a903dae7f
Francis Rath <francis@digitalcreations.no>
2024-08-29 09:51:34 +0200

    Ready for final checking

---
+++ pcb/generate-gerbers.cmd
@@ -0,0 +1,19 @@
+@echo Creating panel
+@kikit panelize ^
+    --layout "grid; rows: 5; cols: 2; space: 2mm" ^
+    --tabs annotation ^
+    --cuts "mousebites; drill: 0.4mm; spacing: 0.6mm; offset: -0.2mm; prolong: 0.5mm" ^
+    --framing "railstb; width: 5mm; space: 3mm;" ^
+    --tooling "3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm" ^
+    --fiducials "3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;" ^
+    --text "simple; text: esphome humitemp; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;" ^
+    --text2 "simple; text: JLCJLCJLCJLC; anchor: mb; voffset: -2.5mm; hjustify: center; vjustify: center;" ^
+    --post "millradius: 1mm" ^
+    esphome-humitemp.kicad_pcb humitemp-production-panel.kicad_pcb
+
+@mkdir humitemp-production-panel-gerbers
+@echo Exporting Gerbers
+@kicad-cli pcb export gerbers -o humitemp-production-panel-gerbers\ humitemp-production-panel.kicad_pcb
+@echo Compressing Gerbers
+@zip -q humitemp-production-panel-gerbers.zip humitemp-production-panel-gerbers\*
+@echo humitemp-production-panel-gerbers.zip ready for upload
---
+++ pcb/panelize.cmd
@@ -0,0 +1,11 @@
+kikit panelize ^
+    --layout "grid; rows: 5; cols: 2; space: 2mm" ^
+    --tabs annotation ^
+    --cuts "mousebites; drill: 0.4mm; spacing: 0.6mm; offset: -0.2mm; prolong: 0.5mm" ^
+    --framing "railstb; width: 5mm; space: 3mm;" ^
+    --tooling "3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm" ^
+    --fiducials "3fid; hoffset: 5mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;" ^
+    --text "simple; text: esphome humitemp; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;" ^
+    --text2 "simple; text: JLCJLCJLCJLC; anchor: mb; voffset: -2.5mm; hjustify: center; vjustify: center;" ^
+    --post "millradius: 1mm" ^
+    esphome-humitemp.kicad_pcb humitemp-production-panel.kicad_pcb
